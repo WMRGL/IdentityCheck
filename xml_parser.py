c321462c248f5d979541147e252589f052bf050f
sarahjamieson <sarah.jamieson@postgrad.manchester.ac.uk>
2019-01-28 14:39:30 +0000

    Create xml_parser.py

---
+++ xml_parser.py
@@ -0,0 +1,259 @@
+"""
+
+ Script to parse XML output from MassArray ExomeQC and generate a VCF for each sample in the results.
+ 
+ To run, e.g.:
+ 
+    python xml_parser.py -xml WMRGL_ExomeQC_validation_Plate1_031218_1_F0008858.xml -bed hg19.bed
+    
+ 
+ Sarah Burns 28 Jan 2019
+
+"""
+
+from xml.etree import ElementTree
+from datetime import datetime
+import re
+import argparse
+import os
+
+
+arg_parser = argparse.ArgumentParser(description='Parses XML from MassArray into VCFs for each sample in results.')
+arg_parser.add_argument('-xml', action='store', help='Path to XML file', required=True)
+arg_parser.add_argument('-bed', action='store', help='Bed file with full SNP info', required=True)
+args = arg_parser.parse_args()
+
+
+class ParseMassArrayXml(object):
+
+    def __init__(self, xml_file, snp_bedfile):
+        self.xml_tree = ElementTree.parse(xml_file)
+        self.snp_file = snp_bedfile
+
+    def get_snps(self):
+        """ Convert SNP bed file into dictionary format.    
+            
+            Output e.g. {
+                            'rs123': {
+                                'chrom': 'chr1', 
+                                'pos': 123, 
+                                'ref': 'A', 
+                                'alt': 'G', 
+                                'gene': 'ABC1', 
+                                'maf': 0.44, 
+                                'genome_build': 'hg19'
+                            }
+                        }
+
+        """
+        d = {}
+        with open(self.snp_file, 'r') as infile:
+            for row in infile:
+                if row:
+                    row_split = row.strip().split('\t')
+                    chrom = row_split[0]
+                    pos = row_split[1]
+                    name = row_split[3].split('|')
+                    snp_id = name[0]
+                    gene = name[1]
+                    ref_allele = name[2]
+                    alt_alleles = name[3]
+                    freq = name[4]
+                    genome = name[5]
+                    d[snp_id] = {
+                        'chrom': chrom,
+                        'pos': pos,
+                        'ref': ref_allele,
+                        'alt': alt_alleles,
+                        'gene': gene,
+                        'maf': freq,
+                        'genome_build': genome
+                    }
+        return d
+
+    def get_analyser_name(self):
+        """ Get name of MassArray analyser to add to VCF header.
+        
+            Output e.g. "MA4-3.3.1.26-iPLEX"
+
+        """
+        for analyser in self.xml_tree.getroot():
+            for child in analyser:
+                if child.tag == 'ms_name':
+                    return child.text
+
+    def get_pcr_sequences(self):
+        """ Get PCR sequences for each SNP in assay and return dictionary.
+        
+            Output e.g. { 'rs123': ['GAGA', 'GAGA'] }
+
+        """
+        d = {}
+        for analyser in self.xml_tree.getroot():
+            for child in analyser:
+                if child.tag == 'all-assays':
+                    for assay in child:
+                        attributes = assay.attrib
+                        assay_id = attributes['id']
+                        if re.match(r'rs\d+', assay_id):
+                            d[assay_id] = [attributes['pcr1'], attributes['pcr2']]
+        return d
+
+    def get_results(self):
+        """ Get SNP results for each sample and return dictionary.
+        
+            Output e.g. {
+                            'D00.00001': {
+                                'rs123': {
+                                    'genotype': '0/1', 
+                                    'quality': 'A'
+                                }
+                            }
+                        }
+
+        """
+        d = {}
+        for analyser in self.xml_tree.getroot():
+            for child in analyser:
+                if child.tag == 'all-records':
+                    for record in child:
+                        attributes = record.attrib
+                        sample = attributes['sampleId']
+                        assay_id = attributes['assayId']
+                        genotype = attributes['genotypeId']
+                        quality = attributes['description'].split('.')[0]
+                        if re.match(r'rs\d+', assay_id):
+                            if sample in d:
+                                if assay_id in d[sample]:
+                                    for allele in list(genotype):
+                                        if allele not in d[sample][assay_id]['genotype']:
+                                            d[sample][assay_id]['genotype'] += allele
+                                    if quality not in d[sample][assay_id]['quality']:
+                                        d[sample][assay_id]['quality'].append(quality)
+                                else:
+                                    d[sample][assay_id] = {'genotype': genotype, 'quality': [quality]}
+                            else:
+                                d[sample] = {assay_id: {'genotype': genotype, 'quality': [quality]}}
+        return d
+
+    def get_snp_call_rate(self):
+        """ Calculate call rates for each SNP and return dictionary. For each SNP, count the number of samples with no 
+            result, returning fraction.
+            
+            Output e.g. { 'rs123': 0.1 }
+
+        """
+        d_temp = {}
+        results = self.get_results()
+        for sample, variants in results.items():
+            for snp, info in variants.items():
+                if snp in d_temp:
+                    d_temp[snp].append(info['genotype'])
+                else:
+                    d_temp[snp] = [info['genotype']]
+        d = {}
+        sample_count = len(results.keys())
+        for key, value in d_temp.items():
+            na_count = len([x for x in value if not x])
+            d[key] = float(na_count) / float(sample_count)
+        return d
+
+    def write_to_vcf(self):
+        """ Run all functions in class to generate VCF file for each sample.
+
+        """
+        
+        # 1. Generate header info
+        date_for_vcf = datetime.now().strftime('%Y%m%d')
+        header_info = [
+            '##fileformat=VCFv4.0',
+            '##fileDate=%s' % date_for_vcf,
+            '##source=%s' % self.get_analyser_name()
+        ]
+        header_parameters = [
+            '##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">',
+            '##FORMAT=<ID=MTQ,Number=1,Type=String,Description="MassArray Typer quality value for SNP call. '
+            'A=Conservative, B=Moderate, C=Aggressive, D=Low Probability, i=Low Intensity. A and B are considered high '
+            'quality scores.">',
+            '##INFO=<ID=PCR,Number=2,Type=String,Description="PCR sequences used in assay.">',
+            '##INFO=<ID=AF,Number=A,Type=Float,Description="Minor allele frequency from population data.">',
+            '##FILTER=<ID=LowCallRate,Description="SNP not called in at least 30% of samples in assay.">'
+        ]
+
+        # 2. Extract info from XML file
+        results = self.get_results()
+        snps = self.get_snps()
+        pcr_sequences = self.get_pcr_sequences()
+        call_rates = self.get_snp_call_rate()
+
+        # 3. For each sample, create VCF, add headers, determine genotype of each SNP and write to file.
+        for sample, variants in results.items():
+
+            with open('massarray_results/%s.vcf' % sample, 'w+') as outfile:
+
+                header_fields = ['CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'INFO', 'FORMAT', str(sample)]
+
+                outfile.write('%s\n' % '\n'.join(header_info))
+                outfile.write('%s\n' % '\n'.join(header_parameters))
+                outfile.write('#%s\n' % '\t'.join(header_fields))
+
+                for snp, info in variants.items():
+
+                    ref_allele = snps[snp]['ref']
+                    alt_alleles = snps[snp]['alt']
+                    alt_list = alt_alleles.split(',')
+
+                    # Genotype formatting matches VCF v4.0 spec where ./. is no call.
+                    gt_list = []
+                    called_genotype = info['genotype']
+                    if not called_genotype:
+                        gt_list = ['.', '.']
+                    elif len(called_genotype) == 1:
+                        called_genotype += called_genotype
+                    for allele in list(called_genotype):
+                        if allele == ref_allele:
+                            gt_list.append(0)
+                        else:
+                            if allele in alt_list:
+                                idx = alt_list.index(allele)
+                                gt_list.append(idx + 1)
+                            else:
+                                raise ValueError(
+                                    'Called genotype %s not provided as possible alt in bed file. Sample %s and SNP '
+                                    '%s %s.' % (called_genotype, sample, snp, alt_alleles)
+                                )
+                    gt = '/'.join([str(x) for x in gt_list])
+                    
+                    # Threshold currently set to 0.3 (70% results have a call).
+                    snp_call_rate = call_rates[snp]
+                    if snp_call_rate >= 0.3:
+                        vcf_filter = 'LowCallRate'
+                    else:
+                        vcf_filter = 'PASS'
+
+                    snp_pcr_seqs = pcr_sequences[snp]
+                    
+                    outfile.write(
+                        '{chr}\t{pos}\t{id}\t{ref}\t{alt}\t.\t{filter}\tAF={af};PCR={pcr};Gene={gene};Build={build}\t'
+                        'GT:MTQ\t{gt}:{qual}\n'.format(
+                            chr=snps[snp]['chrom'],
+                            pos=snps[snp]['pos'],
+                            id=snp,
+                            ref=ref_allele,
+                            alt=alt_alleles,
+                            filter=vcf_filter,
+                            af=snps[snp]['maf'],
+                            pcr=','.join(snp_pcr_seqs),
+                            gene=snps[snp]['gene'],
+                            build=snps[snp]['genome_build'],
+                            gt=gt,
+                            qual=','.join(info['quality'])
+                        )
+                    )
+
+
+# Create directory for output files and run parser.
+os.mkdir('massarray_results')
+xml_parser = ParseMassArrayXml(xml_file=args.xml, snp_bedfile=args.bed)
+xml_parser.write_to_vcf()
+
